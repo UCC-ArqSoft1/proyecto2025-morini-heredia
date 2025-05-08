561309045127fb00b6012c406502805b821fb394
GerMorini <2302130@ucc.edu.ar>
2025-05-08 08:44:42 -0300

    cambios en el directorio app

---
+++ app/router.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	router *gin.Engine
+)
+
+func init() {
+	router = gin.Default()
+	router.Use(cors.Default())
+}
+
+func StartRoute() {
+	log.Info("Iniciando servidor")
+	router.Run(":8080")
+}
