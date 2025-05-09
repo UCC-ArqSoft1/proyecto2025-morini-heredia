964b37a66a73b28ac228dbb5a57d0fc18655bdb9
ivanjalid1 <2312626@ucc.edu.ar>
2025-05-09 01:33:47 -0300

    Se agrega horario inicio y final, que pueden ser null

--- model/actividad.go
+++ model/actividad.go
@@ -1,12 +1,16 @@
 package model
 
+import "time"
+
 type Actividad struct {
-	ID          uint   `gorm:"primaryKey;autoIncrement"`
-	Titulo      string `gorm:"type:varchar(100);not null"`
-	Descripcion string `gorm:"type:text"`
-	Cupo        uint   `gorm:"type:int;not null"`
-	Dia         string `gorm:"type:enum('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo');not null'"`
-	Categoria   string `gorm:"type:varchar(24);not null"`
+	ID            uint      `gorm:"primaryKey;autoIncrement"`
+	Titulo        string    `gorm:"type:varchar(100);not null"`
+	Descripcion   string    `gorm:"type:text"`
+	Cupo          uint      `gorm:"type:int;not null"`
+	Dia           string    `gorm:"type:enum('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo');not null'"`
+	Categoria     string    `gorm:"type:varchar(24);not null"`
+	HorarioInicio time.Time `gorm:"type:timestamp"`
+	HorarioFinal  time.Time `gorm:"type:timestamp"`
 
 	InstructorID uint       `gorm:"not null"` // Clave foránea para Instructor
 	Instructor   Instructor `gorm:"foreignKey:InstructorId"`
