43c675a0432e5004ffe99cc105ae95efdbf948f0
ivanjalid1 <2312626@ucc.edu.ar>
2025-05-27 16:28:10 -0300

    ADD secciones y login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Login from './Login.jsx'
+import Actividades from './Actividades.jsx'
+import Layout from './Layout.jsx'
+import Home from './Home.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <BrowserRouter>
+    <Routes>
+
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/actividades" element={<Actividades />} />
+    </Route>
+    </Routes>
+  </BrowserRouter>
   </StrictMode>,
 )
