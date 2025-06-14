218b144adf1e4a867e4ec993eaca530737814766
GerMorini <2302130@ucc.edu.ar>
2025-06-13 21:33:43 -0300

    dockerizada la app

--- backend/db/db.go
+++ backend/db/db.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"proyecto-integrador/model"
 	"sync"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
@@ -46,13 +47,25 @@ func GetInstance() *gorm.DB {
 		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", db_user, db_pass, db_host, db_port, db_schema)
 		log.Info("Conectando a la base de datos con dsn: ", dsn)
 
-		var err error
-		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
-			Logger: logger.Default.LogMode(logger.Info),
-		})
-		if err != nil {
-			log.Fatalf("Error al conectar a la base de datos: %v", err)
+		// reintentamos conectarnos a la BDD varias veces
+		for i := range 10 {
+			time.Sleep(3 * time.Second)
+			log.Debugf("Intentando conectar (%d/%d)\n", i+1, 10)
+
+			var err error
+			db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+				Logger: logger.Default.LogMode(logger.Info),
+			})
+
+			if err != nil {
+				log.Errorf("Error al conectar a la base de datos: %v", err)
+				log.Error("No se pudo establecer conexion a la BDD")
+				continue
+			}
+
+			break
 		}
+
 		log.Info("Conexion a base de datos establecida")
 	})
 
